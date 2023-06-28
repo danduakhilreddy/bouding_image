d1cdc07c06cf40ba6ce0ca13ea35363318339f28
aharshit <94744372+aharshit@users.noreply.github.com>
2023-06-28 23:51:05 +0530

    Update main.py
    updated 3

--- main.py
+++ main.py
@@ -1,54 +1,47 @@
-import json
-
-# Load JSON data from file
-with open('/Users/danduakhilreddy/Desktop/intern/akhil.json', 'r') as json_file:
-    data = json.load(json_file)
-
-# Extract the value of the desired key
-key_value = data['text']
 import cv2
+import json
+import pytesseract
 
-# Load the given image
-image = cv2.imread('/Users/danduakhilreddy/Desktop/intern/IMG_2830.PNG')
+# Load the image
+image = cv2.imread('img.png')
+
+# Load the JSON file
+with open('akhil.json', 'r') as json_file:
+    data = json.load(json_file)
 # Convert the image to grayscale
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-import pytesseract
+# Perform OCR on the grayscale image
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+result = pytesseract.image_to_data(gray_image, output_type=pytesseract.Output.DICT)
+for entry in data["text"]:
+    # Extract the text
+    text = entry
 
-# Perform text detection
-text = pytesseract.image_to_string(gray_image)
-import re
+    # Split the text into individual words
+    words = text.split()
 
-# Find the bounding box coordinates for the words matching the key value
-matches = re.finditer(r'\b{}\b'.format(re.escape(key_value)), text, flags=re.IGNORECASE)
+    # Initialize the bounding box coordinates for the entire text
+    min_x, min_y, max_x, max_y = float('inf'), float('inf'), 0, 0
 
-bounding_boxes = []
-for match in matches:
-    start = match.start()
-    end = match.end()
+    # Search for each word in the OCR result
+    for word in words:
+        for i in range(len(result['text'])):
+            if result['text'][i].strip() == word.strip():
+                # Extract the coordinates of the corresponding OCR result
+                x, y, width, height = result['left'][i], result['top'][i], result['width'][i], result['height'][i]
+                min_x = min(min_x, x)
+                min_y = min(min_y, y)
+                max_x = max(max_x, x + width)
+                max_y = max(max_y, y + height)
+                break
 
-    # Perform word tokenization to get the position of the matched words
-    words = text[:start].split()
-    word_positions = sum(len(word) for word in words) + len(words)
+            # Draw the bounding box on the image
+    cv2.rectangle(image, (min_x, min_y), (max_x, max_y), (0, 255, 0), 2)
 
-    # Find the bounding box coordinates based on the word positions
-    (x, y, width, height), _ = pytesseract.image_to_boxes(gray_image)
-    x_start, y_start, x_end, y_end = _, _, _, _
-    for i, (x_pos, y_pos) in enumerate(zip(x, y)):
-        if i == word_positions:
-            x_start, y_start = x_pos, y_pos
-        elif i == word_positions + len(key_value):
-            x_end, y_end = x_pos, y_pos
-            break
 
-    bounding_boxes.append((x_start, y_start, x_end, y_end))
-# Mark the bounding boxes on the original image
-for (x_start, y_start, x_end, y_end) in bounding_boxes:
-    cv2.rectangle(image, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
-# Display the marked image
-cv2.imshow("Image with Marked Words", image)
+# Display the image with bounding boxes
+cv2.imshow('Image with Bounding Boxes', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-# Save the marked image
 cv2.imwrite("image_with_marked_words.jpg", image)
